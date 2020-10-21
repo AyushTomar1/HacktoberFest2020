8e144d68e5119c3abe6a478514e30206b57bf8e2
AyushTomar1 <73213195+AyushTomar1@users.noreply.github.com>
2020-10-21 11:15:13 +0530

    Update Prime___checker.py

--- Prime___checker.py
+++ Prime___checker.py
@@ -1,10 +1,9 @@
-n = int(input())
-if n > 1: 
-    for i in range(2, n): 
-        if (n % i) == 0: 
-            print(n, "not_a_prime") 
-            break
-    else: 
-        print(n, "is_a_prime") 
-else: 
-   print(n, "not_a_prime") 
+num = int(input())
+l=[]
+for i in range(1,num+1):
+	if(num%i==0):
+		l.append(i)
+if(len(l)==2):
+	print(num, 'is a prime number')
+else:
+	print(num, " Is Not a prime Number")
